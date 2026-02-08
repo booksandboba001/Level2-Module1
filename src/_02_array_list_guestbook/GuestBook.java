f385003512616564d19d411e51b0478355433c17
christianawuornos <christianawuornos@192.168.1.79>
2026-02-08 14:31:26 -0800

    May Ortho live out his years as my human big brother in my headcanon!!!

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -2,21 +2,62 @@ package _02_array_list_guestbook;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GuestBook implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
-	GuestBook(){
+	JButton enter= new JButton();
+	JTextField adds = new JTextField("Add Name");
+	ArrayList<String> list=new ArrayList<String>();
+	int numOfGuests;
+	public void run(){
 		button1.setText("Add Name");
 		button2.setText("View Names");
+		enter.setText("Enter");
+		button1.setVisible(true);
+		button2.setVisible(true);
+		panel.add(button1);
+		panel.add(button2);
+		frame.add(panel);
+		frame.setVisible(true);
+		panel.setVisible(true);
 		button1.addActionListener(this);
 		button2.addActionListener(this);
+		frame.pack();
+	}
+	public void addNames() {
+		panel.remove(button2);
+		panel.add(adds);
+		panel.remove(button1);
+		panel.add(enter);
+		adds.setVisible(true);
+		enter.setVisible(true);
+		enter.addActionListener(this);
+		frame.pack();
+		
+	}
+	public void gList() {
+		for (int i =0; i<=numOfGuests-1;i++) {
+			JOptionPane.showMessageDialog(null,"Guest "+i+": "+list.get(i));
+		}
+	}
+	public void entered() {
+		panel.remove(adds);
+		panel.remove(enter);
+		panel.add(button1);
+		panel.add(button2);
+		list.add(adds.getText());
+		numOfGuests++;
+		frame.pack();
 	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
@@ -31,7 +72,13 @@ public class GuestBook implements ActionListener {
 		// TODO Auto-generated method stub
 		JButton buttonPressed = (JButton) e.getSource();
 		if (buttonPressed==button1) {
-			
+			addNames();
+		}
+		if(buttonPressed==button2) {
+			gList();
+		}
+		if(buttonPressed==enter) {
+			entered();
 		}
 	}
 }
